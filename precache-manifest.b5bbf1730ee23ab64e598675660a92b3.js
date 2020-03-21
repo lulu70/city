57e9957935cbd201dfaa0e6d69da329b7d263479
lior <lulu70@users.noreply.github.com>
2020-03-21 13:28:18 +0100

    Add files via upload

---
+++ precache-manifest.b5bbf1730ee23ab64e598675660a92b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7818d8fbd1c0e145158e274981e077f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8be85fa220a61761d4ba",
+    "url": "/static/css/main.ebd74ed0.chunk.css"
+  },
+  {
+    "revision": "4b78c7dea5b02c6d5be3",
+    "url": "/static/js/2.ced4862e.chunk.js"
+  },
+  {
+    "revision": "9aea971713c6773bef69d3e2a3c91fd5",
+    "url": "/static/js/2.ced4862e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8be85fa220a61761d4ba",
+    "url": "/static/js/main.161665ff.chunk.js"
+  },
+  {
+    "revision": "ea9b3bb0f19b02c38126",
+    "url": "/static/js/runtime-main.b869fbd0.js"
+  },
+  {
+    "revision": "9283ef3f53017f897fb2151ca7b94e8e",
+    "url": "/static/media/eye.9283ef3f.png"
+  },
+  {
+    "revision": "b72995ae6ba51ead99a9f7cd08e798af",
+    "url": "/static/media/image.b72995ae.jpg"
+  }
+]);
